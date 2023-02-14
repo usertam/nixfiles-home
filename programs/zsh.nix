92ecdf1ddc58a7a3268859376f21834adf7d4bc6
Samuel Tam <heiyiutam@gmail.com>
2023-02-14 15:43:31 +0800

    programs/zsh: rework omz search behaviour

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -1,17 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
+    initExtra = ''
+      autoload -U up-line-or-beginning-search
+      autoload -U down-line-or-beginning-search
+      zle -N up-line-or-beginning-search
+      zle -N down-line-or-beginning-search
+      bindkey "^[[A" up-line-or-beginning-search
+      bindkey "^[[B" down-line-or-beginning-search
+    '';
     localVariables.SPACESHIP_EXEC_TIME_PRECISION = 0;
-    plugins = [
-      {
-        name = "spaceship-prompt";
-        src = pkgs.spaceship-prompt;
-        file = "share/zsh/themes/spaceship.zsh-theme";
-      }
-    ];
+    plugins = lib.singleton {
+      name = "spaceship-prompt";
+      src = pkgs.spaceship-prompt;
+      file = "share/zsh/themes/spaceship.zsh-theme";
+    };
   };
 }
