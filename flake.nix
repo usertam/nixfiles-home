45adaeeb3e87964abeb674df6700caa2761469a5
Samuel Tam <heiyiutam@gmail.com>
2023-02-20 04:15:48 +0800

    flake.nix: replace system attribute with forAllSystems

--- flake.nix
+++ flake.nix
@@ -14,27 +14,18 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-index-db = {
-      url = "github:usertam/nix-index-db/standalone/nixpkgs-unstable";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    plasma-manager = {
-      url = "github:pjones/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-db.url = "github:usertam/nix-index-db/standalone/nixpkgs-unstable";
+    nix-index-db.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs:
-    let
-      username = "tam";
-      system = "x86_64-linux";
-      desktop = true;
-    in {
+  outputs = inputs: let
+    forAllSystems = with inputs.nixpkgs.lib; genAttrs platforms.unix;
+    username = "tam";
+    desktop = true;
+  in {
+    packages = forAllSystems (system: {
       homeConfigurations.${username} = inputs.home-manager.lib.homeManagerConfiguration rec {
         pkgs = inputs.nixpkgs.legacyPackages.${system};
         extraSpecialArgs = {
@@ -61,5 +52,6 @@
           ./programs/vlc.nix
         ]);
       };
-    };
+    });
+  };
 }
