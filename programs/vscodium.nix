65f731ae62284d29c47eb9e4009f73b955d35e07
usertam <heiyiutam@gmail.com>
2023-06-04 00:33:33 +0800

    programs/vscodium: update, add extension mrmlnc-vscode-apache

--- programs/vscodium.nix
+++ programs/vscodium.nix
@@ -21,8 +21,8 @@
         mktplcRef = {
           name = "copilot-nightly";
           publisher = "github";
-          version = "1.82.21";
-          sha256 = "sha256-m6HDRp/LyyqVHsu1wvmkur5yBWSK68b5JCeH6QQF+D0=";
+          version = "1.88.136";
+          sha256 = "sha256-5X2KTJs1qIwGzDEbzgXh7SDHTIQJblSt5sUT3deTXwc=";
         };
         meta.license = lib.licenses.unfree;
       };
@@ -30,10 +30,19 @@
         mktplcRef = {
           name = "copilot-labs";
           publisher = "github";
-          version = "0.12.791";
-          sha256 = "sha256-3StswisTiG1e+LZeAuquIXlqaFj0Lzk4WNy+6Af4giw=";
+          version = "0.14.884";
+          sha256 = "sha256-44t4qdRjw/sdAmO6uW9CaLzs0hJcK+uQnpalCNB8AdM=";
         };
         meta.license = lib.licenses.unfree;
+      };
+      mrmlnc-vscode-apache = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+        mktplcRef = {
+          name = "vscode-apache";
+          publisher = "mrmlnc";
+          version = "1.2.0";
+          sha256 = "sha256-wXxgrYnX7YxKQJfzgyV7kpG7wJYE4NOPScT4/BeCv5s=";
+        };
+        meta.license = lib.licenses.mit;
       };
     in with pkgs.vscode-extensions; [
       antfu.icons-carbon
@@ -49,6 +58,7 @@
       redhat.vscode-yaml
       redhat.vscode-xml
       llvm-vs-code-extensions.vscode-clangd
+      mrmlnc-vscode-apache
     ] ++ lib.optionals pkgs.stdenv.isLinux [
       ms-vscode.cpptools
       ms-python.python
@@ -84,6 +94,13 @@
       "redhat.telemetry.enabled" = false;
       # Disable updates.
       "update.mode" = "none";
+      # github copilot.
+      "github.copilot.enable" = {
+        "*" = true;
+        "plaintext" = false;
+        "markdown" = true;
+        "scminput" = false;
+      };
     };
   };
 }
