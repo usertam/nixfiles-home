4120e7a0aea093aa4c52dce414e6f0099b55a47c
usertam <heiyiutam@gmail.com>
2023-06-15 22:10:28 +0800

    programs/common: remove unused, add nixos set

--- programs/common.nix
+++ programs/common.nix
@@ -5,25 +5,24 @@
   nixpkgs.config.allowUnfreePredicate = pkg: true;
 
   home.packages = with pkgs; [
-    axel
-    clang
-    ccache
     coreutils
     diffutils
     discord-canary
+    file
     findutils
+    git
     gnugrep
     gnupg
     gnused
     graphicsmagick
     home-manager
+    htop
     imagemagick
+    kitty.terminfo
     lesspipe
     nano
     nix-index
     openssh
-    podman-compose
-    podman-tui
     poppler_utils
     python3
     qrencode
