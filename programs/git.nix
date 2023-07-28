327645574199daea1669ffe913071fda1ec68d97
usertam <heiyiutam@gmail.com>
2023-07-28 15:22:44 +0800

    programs/git: enable rerere

--- programs/git.nix
+++ programs/git.nix
@@ -9,9 +9,14 @@
       key = "A6C5AC43";
       signByDefault = true;
     };
-    extraConfig.init.defaultBranch = "master";
-    extraConfig.core.editor = "nano";
-    extraConfig.core.excludesfile = builtins.toFile "gitignore" ".DS_Store\n";
-    extraConfig.core.pager = "less -+X";
+    extraConfig = {
+      core = {
+        editor = "nano";
+        excludesfile = builtins.toFile "gitignore" ".DS_Store\n";
+        pager = "less -+X";
+      };
+      init.defaultBranch = "master";
+      rerere.enabled = true;
+    };
   };
 }
