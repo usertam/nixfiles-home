ad216f383d0a38c9a7bcb92606fa6262a457bae3
usertam <heiyiutam@gmail.com>
2024-01-09 17:48:34 +0800

    programs/fonts: refactor, add brass mono

--- programs/fonts.nix
+++ programs/fonts.nix
@@ -3,13 +3,13 @@
 {
   home.packages = let
 
-    # non-variable noto cjk sans.
-    noto-fonts-cjk-sans-static = pkgs.noto-fonts-cjk-sans.overrideAttrs (final: prev: {
+    # Static Noto CJK Sans.
+    noto-fonts-cjk-sans-static = pkgs.noto-fonts-cjk-sans.overrideAttrs (prev: {
       src = pkgs.fetchFromGitHub {
         owner = "googlefonts";
         repo = "noto-cjk";
         rev = "9f7f3c38eab63e1d1fddd8d50937fe4f1eacdb1d";
-        sha256 = "sha256-pNC/WJCYHSlU28E/CSFsrEMbyCe/6tjevDlOvDK9RwU=";
+        hash = "sha256-pNC/WJCYHSlU28E/CSFsrEMbyCe/6tjevDlOvDK9RwU=";
         sparseCheckout = [ "Sans/OTC" ];
       };
       installPhase = ''
@@ -17,13 +17,13 @@
       '';
     });
 
-    # non-variable noto cjk serif.
-    noto-fonts-cjk-serif-static = pkgs.noto-fonts-cjk-serif.overrideAttrs (final: prev: {
+    # Static Noto CJK Serif.
+    noto-fonts-cjk-serif-static = pkgs.noto-fonts-cjk-serif.overrideAttrs (prev: {
       src = pkgs.fetchFromGitHub {
         owner = "googlefonts";
         repo = "noto-cjk";
         rev = "9f7f3c38eab63e1d1fddd8d50937fe4f1eacdb1d";
-        sha256 = "sha256-Iy4lmWj5l+/Us/dJJ/Jl4MEojE9mrFnhNQxX2zhVngY=";
+        hash = "sha256-Iy4lmWj5l+/Us/dJJ/Jl4MEojE9mrFnhNQxX2zhVngY=";
         sparseCheckout = [ "Serif/OTC" ];
       };
       installPhase = ''
@@ -31,15 +31,29 @@
       '';
     });
 
-    # san francisco pro family.
+    # Brass Mono.
+    brass-mono = pkgs.stdenvNoCC.mkDerivation rec {
+      pname = "brass-mono";
+      version = "1.100";
+      src = pkgs.fetchzip {
+        url = "https://github.com/fonsecapeter/brass_mono/releases/download/v${version}/BrassMono.zip";
+        hash = "sha256-viGI+vYvQ3D3PucZuw6m3dW0UqgdpWzXFDvsScC8IDQ=";
+        stripRoot = false;
+      };
+      installPhase = ''
+        install -Dm444 -t $out/share/fonts/truetype *.ttf
+      '';
+    };
+
+    # San Francisco Pro.
     sf-pro = pkgs.stdenvNoCC.mkDerivation {
       pname = "sf-pro";
-      version = "230615"; # curl -v ... 2>&1 | grep Last-Modified
+      version = "1696523982"; # curl -v ... 2>&1 | grep Last-Modified; date --date=... +%s
       src = pkgs.fetchurl {
         url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-        sha256 = "sha256-WG0nLn/Giiv0DT8zUwTiWuv/I23RqMSxJsGUbrQzCqc=";
+        hash = "sha256-WG0nLn/Giiv0DT8zUwTiWuv/I23RqMSxJsGUbrQzCqc=";
       };
-      buildInputs = [ pkgs.p7zip ];
+      nativeBuildInputs = [ pkgs.p7zip ];
       unpackPhase = ''
         7z x $src
         7z x 'SFProFonts/SF Pro Fonts.pkg'
@@ -52,6 +66,7 @@
     };
 
   in with pkgs; [
+    brass-mono
     fira-code
     fira-code-symbols
     libertinus
