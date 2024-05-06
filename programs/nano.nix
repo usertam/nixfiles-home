808f635d6ad9c5e0d25b5b33eb2a042f4d5cd3d4
usertam <heiyiutam@gmail.com>
2024-05-07 07:19:08 +0800

    programs/nano: rewrite to home.file.*.text syntax

--- programs/nano.nix
+++ programs/nano.nix
@@ -3,10 +3,8 @@
 {
   home.packages = [ pkgs.nano ];
 
-  home.file.".nanorc" = {
-    source = pkgs.writeText "nanorc" ''
-      include "${pkgs.nano}/share/nano/*.nanorc"
-      include "${pkgs.nano}/share/nano/extra/*.nanorc"
-    '';
-  };
+  home.file.".nanorc".text = ''
+    include "${pkgs.nano}/share/nano/*.nanorc"
+    include "${pkgs.nano}/share/nano/extra/*.nanorc"
+  '';
 }
