b960641f12e44a75f6dd5769941b798882bffe52
usertam <heiyiutam@gmail.com>
2024-12-28 16:47:38 +0800

    programs/nix: add what-the-hack patches

--- programs/nix.nix
+++ programs/nix.nix
@@ -2,8 +2,15 @@
 
 {
   nix = {
-    # Use bleeding-edge version of nix.
-    package = pkgs.nixVersions.git;
+    # Use bleeding-edge version of nix, patched with usertam/nix/what-the-hack.
+    package = pkgs.nixVersions.git.overrideAttrs (prev: {
+      patches = (prev.patches or []) ++ lib.singleton (pkgs.fetchpatch {
+        url = "https://github.com/NixOS/nix/compare/bff9296...usertam:nix:db13dbb.patch";
+        hash = "sha256-LmLDPaDsUCNFvvFvx8qcC2rWJzxHkcn984HqW+38RHU=";
+      });
+      doCheck = false;
+      doInstallCheck = false;
+    });
 
     # For insane system nix config.
     settings.extra-experimental-features = [ "nix-command" "flakes" ];
