02fa024d27037ffd2241841873c12ea2262607cc
usertam <code@usertam.dev>
2025-03-01 21:37:40 +0800

    programs/zsh.nix: pin gnugrep and gnused in buildInputs

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -99,43 +99,44 @@
         # Enable spaceship theme.
         name = "spaceship-prompt";
         src = pkgs.spaceship-prompt.overrideAttrs (prev: {
-        patches = (prev.patches or []) ++ lib.singleton (pkgs.writeText "customize-for-new-nix-shell.patch" ''
-          diff --git a/sections/nix_shell.zsh b/sections/nix_shell.zsh
-          index 3d35db052..77cdfccfe 100644
-          --- a/sections/nix_shell.zsh
-          +++ b/sections/nix_shell.zsh
-          @@ -10,10 +10,11 @@
-           
-           SPACESHIP_NIX_SHELL_SHOW="''${SPACESHIP_NIX_SHELL_SHOW=true}"
-           SPACESHIP_NIX_SHELL_ASYNC="''${SPACESHIP_NIX_SHELL_ASYNC=false}"
-          +SPACESHIP_NIX_SHELL_VERSION="''${SPACESHIP_NIX_SHELL_VERSION=false}"
-           SPACESHIP_NIX_SHELL_PREFIX="''${SPACESHIP_NIX_SHELL_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
-           SPACESHIP_NIX_SHELL_SUFFIX="''${SPACESHIP_NIX_SHELL_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
-           SPACESHIP_NIX_SHELL_SYMBOL="''${SPACESHIP_NIX_SHELL_SYMBOL="❄ "}"
-          -SPACESHIP_NIX_SHELL_COLOR="''${SPACESHIP_NIX_SHELL_COLOR="yellow"}"
-          +SPACESHIP_NIX_SHELL_COLOR="''${SPACESHIP_NIX_SHELL_COLOR="blue"}"
-           
-           # ------------------------------------------------------------------------------
-           # Section
-          @@ -23,12 +24,12 @@ SPACESHIP_NIX_SHELL_COLOR="''${SPACESHIP_NIX_SHELL_COLOR="yellow"}"
-           spaceship_nix_shell() {
-             [[ $SPACESHIP_NIX_SHELL_SHOW == false ]] && return
-           
-          -  [[ -z "$IN_NIX_SHELL" ]] && return
-          +  [[ -z "$IN_NIX_SHELL" ]] && ! (echo "$PATH" | grep -q '/nix/store') && return
-           
-          -  if [[ -z "$name" || "$name" == "" ]] then
-          -    display_text="$IN_NIX_SHELL"
-          +  if [[ $SPACESHIP_NIX_SHELL_VERSION == true ]]; then
-          +    display_text="$(echo "$PATH" | ${pkgs.coreutils}/bin/grep -Po '/nix/store.*?/bin' | uniq | ${pkgs.gnused}/bin/sed ':a; s+/.\{42\}-++g; s+/bin++g; s/\n/, /g; N; ba;')"
-             else
-          -    display_text="$IN_NIX_SHELL ($name)"
-          +    display_text="$(echo "$PATH" | ${pkgs.coreutils}/bin/grep -Po '/nix/store.*?/bin' | uniq | ${pkgs.gnused}/bin/sed ':a; s+/.\{42\}-++g; s+/bin++g; s/-[0-9][0-9.]*//g; s/\n/, /g; N; ba;')"
-             fi
-           
-             # Show prompt section
-          '');
-        });
+          buildInputs = with pkgs; (prev.buildInputs or []) ++ [ gnugrep gnused ];
+          patches = (prev.patches or []) ++ lib.singleton (pkgs.writeText "customize-for-new-nix-shell.patch" ''
+            diff --git a/sections/nix_shell.zsh b/sections/nix_shell.zsh
+            index 3d35db052..77cdfccfe 100644
+            --- a/sections/nix_shell.zsh
+            +++ b/sections/nix_shell.zsh
+            @@ -10,10 +10,11 @@
+             
+             SPACESHIP_NIX_SHELL_SHOW="''${SPACESHIP_NIX_SHELL_SHOW=true}"
+             SPACESHIP_NIX_SHELL_ASYNC="''${SPACESHIP_NIX_SHELL_ASYNC=false}"
+            +SPACESHIP_NIX_SHELL_VERSION="''${SPACESHIP_NIX_SHELL_VERSION=false}"
+             SPACESHIP_NIX_SHELL_PREFIX="''${SPACESHIP_NIX_SHELL_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
+             SPACESHIP_NIX_SHELL_SUFFIX="''${SPACESHIP_NIX_SHELL_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
+             SPACESHIP_NIX_SHELL_SYMBOL="''${SPACESHIP_NIX_SHELL_SYMBOL="❄ "}"
+            -SPACESHIP_NIX_SHELL_COLOR="''${SPACESHIP_NIX_SHELL_COLOR="yellow"}"
+            +SPACESHIP_NIX_SHELL_COLOR="''${SPACESHIP_NIX_SHELL_COLOR="blue"}"
+             
+             # ------------------------------------------------------------------------------
+             # Section
+            @@ -23,12 +24,12 @@ SPACESHIP_NIX_SHELL_COLOR="''${SPACESHIP_NIX_SHELL_COLOR="yellow"}"
+             spaceship_nix_shell() {
+               [[ $SPACESHIP_NIX_SHELL_SHOW == false ]] && return
+             
+            -  [[ -z "$IN_NIX_SHELL" ]] && return
+            +  [[ -z "$IN_NIX_SHELL" ]] && ! (echo "$PATH" | grep -q '/nix/store') && return
+             
+            -  if [[ -z "$name" || "$name" == "" ]] then
+            -    display_text="$IN_NIX_SHELL"
+            +  if [[ $SPACESHIP_NIX_SHELL_VERSION == true ]]; then
+            +    display_text="$(echo "$PATH" | ${pkgs.gnugrep}/bin/grep -Po '/nix/store.*?/bin' | uniq | ${pkgs.gnused}/bin/sed ':a; s+/.\{42\}-++g; s+/bin++g; s/\n/, /g; N; ba;')"
+               else
+            -    display_text="$IN_NIX_SHELL ($name)"
+            +    display_text="$(echo "$PATH" | ${pkgs.gnugrep}/bin/grep -Po '/nix/store.*?/bin' | uniq | ${pkgs.gnused}/bin/sed ':a; s+/.\{42\}-++g; s+/bin++g; s/-[0-9][0-9.]*//g; s/\n/, /g; N; ba;')"
+               fi
+             
+               # Show prompt section
+            '');
+          });
         file = "share/zsh/themes/spaceship.zsh-theme";
       }
       {
