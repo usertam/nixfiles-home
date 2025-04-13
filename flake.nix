07def268f43dd19eaa9333509273ecc8a2c4706d
usertam <code@usertam.dev>
2025-04-13 22:36:30 +0800

    flake.lock: try to pin nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "usertam's home-manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix4vscode.url = "github:nix-community/nix4vscode";
