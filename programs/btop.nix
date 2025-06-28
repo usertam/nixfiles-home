256279a6fcdc3771cbbfa244488de28098d66380
usertam <code@usertam.dev>
2025-06-28 23:45:36 +0800

    programs/btop: update btop patch

--- programs/btop.nix
+++ programs/btop.nix
@@ -6,8 +6,8 @@
     package = pkgs.btop.overrideAttrs (prev: {
       patches = (prev.patches or []) ++ [
         (pkgs.fetchpatch {
-          url = "https://github.com/usertam/btop/commit/0d9dcbf5c977ca092ac6a5f550e0b7b9937ac6ba.patch";
-          hash = "sha256-OU0m4UnzmhapPBuhm4b7cpS8bE5gnT3/cNTwHSmYinI=";
+          url = "https://github.com/usertam/btop/commit/68aed42f33071071d1d9bc03f3be5fb25fa5dfc6.patch";
+          hash = "sha256-RBz+6XZAJZrNXAlHGO5eKtuQl4/9ZMa0OhQtm6g6oGE=";
         })
       ];
     });
