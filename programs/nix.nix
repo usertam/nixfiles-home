19007f92f787c2d08692f4353bcfbf29ff94c1b6
usertam <code@usertam.dev>
2025-09-25 15:22:12 +0800

    programs/nix: update package patch

--- programs/nix.nix
+++ programs/nix.nix
@@ -5,8 +5,8 @@
     # Use bleeding-edge version of nix, patched with what-the-hack.
     package = (pkgs.nixVersions.nixComponents_2_29.appendPatches [
       (pkgs.fetchpatch {
-        url = "https://github.com/NixOS/nix/compare/master...usertam:nix:ad2869d.patch";
-        hash = "sha256-nG403Ex/w3CnsNd7+c0HFDuwbe68OazvGuASFXYZZI8=";
+        url = "https://github.com/NixOS/nix/compare/master...usertam:nix:0dab9b9f003d5354e2af2227464d5962c740f74f.patch";
+        hash = "sha256-Xudx14849LHbvusVVN1J2PYwLLLE20Jt2OXiYKjlQdg=";
       })
     ]).nix-everything.overrideAttrs (prev: {
       doCheck = false;
