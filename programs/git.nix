27aa589189d3d559690a770e7baef167b8840fda
usertam <code@usertam.dev>
2025-11-03 01:10:14 +0800

    programs/git: use new programs.git.settings syntax

--- programs/git.nix
+++ programs/git.nix
@@ -3,14 +3,16 @@
 {
   programs.git = {
     enable = true;
-    userEmail = "code@usertam.dev";
-    userName = "usertam";
     signing = {
       key = "EC4EE4903C8236982CABD2062D8760B0229E2560";
       signByDefault = true;
     };
     lfs.enable = true;
-    extraConfig = {
+    settings = {
+      user = {
+        email = "code@usertam.dev";
+        name = "usertam";
+      };
       core = {
         editor = "nano";
         excludesfile = builtins.toFile "gitignore" ''
